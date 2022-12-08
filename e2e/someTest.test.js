bfc9335f07943b2c3c225c4521f7fd725ce217f0
UladzislauAharodnikau <uladzislau.aharodnikau@innowise-group.com>
2022-12-08 17:08:40 +0300

    - added detox package and configured it; - added first test for init app;

---
+++ e2e/someTest.test.js
@@ -0,0 +1,10 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line no-undef
+    // await device.launchApp();
+  });
+
+  beforeEach(async () => {});
+
+  it('dummy test example', async () => {});
+});
